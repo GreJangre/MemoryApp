4abb0cef98ebad6306d7790af99a265cc1554ea5
GreJangre <dlatjdwn8824@naver.com>
2019-02-20 02:55:09 +0900

    데이터 모델 작업 커스텀 클래스 작성

---
+++ MemoryApp/MemoData.swift
@@ -0,0 +1,17 @@
+//
+//  MemoData.swift
+//  MemoryApp
+//
+//  Created by 임성주 on 20/02/2019.
+//  Copyright © 2019 jangre. All rights reserved.
+//
+
+import UIKit
+
+class MemoData {
+    var memoIdx : Int?
+    var title : String?
+    var contents : String?
+    var image : UIImage?
+    var regdate : Date?
+}
