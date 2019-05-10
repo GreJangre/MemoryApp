9f62f013e4427e0d757a4990c382ddd183db03df
GreJangre <dlatjdwn8824@naver.com>
2019-05-10 19:26:54 +0900

    계정 관련 연산 프로퍼티 구현

---
+++ MemoryApp/UserInfoManager.swift
@@ -0,0 +1,100 @@
+//
+//  UserInfoManager.swift
+//  MemoryApp
+//
+//  Created by 임성주 on 10/05/2019.
+//  Copyright © 2019 jangre. All rights reserved.
+//
+
+import UIKit
+
+struct UserInfoKey {
+    static let loginId = "LOGINID"
+    static let account = "ACCOUNT"
+    static let name = "NAME"
+    static let profile = "PROFILE"
+}
+
+class UserInfoManager {
+    var loginid: Int {
+        get {
+            return UserDefaults.standard.integer(forKey: UserInfoKey.loginId)
+        }
+        set (v) {
+            let ud = UserDefaults.standard
+            ud.set(v, forKey: UserInfoKey.loginId)
+            ud.synchronize()
+        }
+    }
+    
+    var account: String? {
+        get {
+            return UserDefaults.standard.string(forKey: UserInfoKey.account)
+        }
+        set (v) {
+            let ud = UserDefaults.standard
+            ud.set(v, forKey: UserInfoKey.account)
+            ud.synchronize()
+        }
+    }
+    
+    var name: String? {
+        get {
+            return UserDefaults.standard.string(forKey: UserInfoKey.name)
+        }
+        set (v) {
+            let ud = UserDefaults.standard
+            ud.set(v, forKey: UserInfoKey.name)
+            ud.synchronize()
+        }
+    }
+    
+    var profile: UIImage? {
+        get {
+            let ud = UserDefaults.standard
+            if let _profile = ud.data(forKey: UserInfoKey.profile) {
+                return UIImage(data: _profile)
+            } else {
+                return UIImage(named: "account.jpg")
+            }
+        }
+        set (v) {
+            if v != nil {
+                let ud = UserDefaults.standard
+                ud.set(v!.pngData(), forKey: UserInfoKey.profile)
+                ud.synchronize()
+            }
+        }
+    }
+    
+    var isLogin: Bool {
+        if self.loginid == 0 || self.account == nil {
+            return false
+        } else {
+            return true
+        }
+    }
+    
+    func login(account: String, passwd: String) -> Bool {
+        if account.isEqual("sqlpro@naver.com") && passwd.isEqual("1234") {
+            let ud = UserDefaults.standard
+            ud.set(100, forKey: UserInfoKey.loginId)
+            ud.set(account, forKey: UserInfoKey.account)
+            ud.set("장그레", forKey: UserInfoKey.name)
+            ud.synchronize()
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func logout() -> Bool {
+        let ud = UserDefaults.standard
+        ud.removeObject(forKey: UserInfoKey.loginId)
+        ud.removeObject(forKey: UserInfoKey.account)
+        ud.removeObject(forKey: UserInfoKey.name)
+        ud.removeObject(forKey: UserInfoKey.profile)
+        ud.synchronize()
+        return true
+    }
+}
