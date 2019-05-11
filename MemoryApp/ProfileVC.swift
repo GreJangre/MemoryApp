133f7a9c2b4694400ddf7efb0f80789c2b92cb75
GreJangre <dlatjdwn8824@naver.com>
2019-05-11 18:17:26 +0900

    프로필 화면 구현

--- MemoryApp/ProfileVC.swift
+++ MemoryApp/ProfileVC.swift
@@ -12,6 +12,7 @@ class ProfileVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     let profileImage = UIImageView()
     let tv = UITableView()
+    let uinfo = UserInfoManager()
     
     // MARK: Override
     override func viewDidLoad() {
@@ -19,6 +20,40 @@ class ProfileVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         
         let backBtn = UIBarButtonItem(title: "닫기", style: .plain, target: self, action: #selector(close(_:)))
         self.navigationItem.leftBarButtonItem = backBtn
+        
+        let bg = UIImage(named: "profile-bg")
+        let bgImage = UIImageView(image: bg)
+        
+        bgImage.frame.size = CGSize(width: bgImage.frame.size.width, height: bgImage.frame.size.height)
+        bgImage.center = CGPoint(x: self.view.frame.width / 2, y: 40)
+        
+        bgImage.layer.cornerRadius = bgImage.frame.size.width / 2
+        bgImage.layer.borderWidth = 0
+        bgImage.layer.masksToBounds = true
+        
+        self.view.addSubview(bgImage)
+        
+        // 프로필 이미지
+        let image = UIImage(named: "account.jpg")
+        
+        self.profileImage.image = image
+        self.profileImage.frame.size = CGSize(width: 100, height: 100)
+        self.profileImage.center = CGPoint(x: self.view.frame.width / 2, y: 270)
+        
+        self.profileImage.layer.cornerRadius = self.profileImage.frame.width / 2
+        self.profileImage.layer.borderWidth = 0
+        self.profileImage.layer.masksToBounds = true
+        
+        self.view.addSubview(self.profileImage)
+        
+        // 프로필 내용
+        self.tv.frame = CGRect(x: 0, y: self.profileImage.frame.origin.y + self.profileImage.frame.size.height + 20, width: self.view.frame.width, height: 100)
+        self.tv.dataSource = self
+        self.tv.delegate = self
+        
+        self.view.addSubview(self.tv)
+        
+        self.navigationController?.navigationBar.isHidden = true
     }
     
     // MARK: Data source
@@ -27,13 +62,28 @@ class ProfileVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
+        let cell = UITableViewCell(style: .value1, reuseIdentifier: "cell")
         
+        cell.textLabel?.font = UIFont.systemFont(ofSize: 14)
+        cell.detailTextLabel?.font = UIFont.systemFont(ofSize: 13)
+        cell.accessoryType = .disclosureIndicator
+        
+        switch indexPath.row {
+        case 0:
+            cell.textLabel?.text = "이름"
+            cell.detailTextLabel?.text = "장그레"
+        case 1:
+            cell.textLabel?.text = "계정"
+            cell.detailTextLabel?.text = "jangre@naver.com"
+        default:
+            ()
+        }
         return cell
     }
     
-    // MARK: objc
+    // MARK: @objc
     @objc func close(_ sender: Any) {
         self.presentingViewController?.dismiss(animated: true)
     }
+    
 }
