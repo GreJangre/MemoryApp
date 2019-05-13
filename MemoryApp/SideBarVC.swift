274faba9115a4ea83aa815b7350dfbc783d8bdc1
GreJangre <dlatjdwn8824@naver.com>
2019-05-14 00:00:12 +0900

    더미 데이터 삭제

--- MemoryApp/SideBarVC.swift
+++ MemoryApp/SideBarVC.swift
@@ -9,6 +9,7 @@
 import UIKit
 class SideBarVC: UITableViewController {
     
+    let uinfo = UserInfoManager()
     let nameLabel = UILabel()
     let emailLabel = UILabel()
     let profileImage = UIImageView()
@@ -30,33 +31,31 @@ class SideBarVC: UITableViewController {
         self.tableView.tableHeaderView = headerView
         
         self.nameLabel.frame = CGRect(x: 70, y: 15, width: 100, height: 30)
-        self.nameLabel.text = "그래그래 장그레"
         self.nameLabel.textColor = UIColor.white
         self.nameLabel.font = UIFont.boldSystemFont(ofSize: 15)
         self.nameLabel.backgroundColor = UIColor.clear
-        
         headerView.addSubview(self.nameLabel)
         
         self.emailLabel.frame = CGRect(x: 70, y: 30, width: 100, height: 30)
-        self.emailLabel.text = "jangre@naver.com"
         self.emailLabel.textColor = UIColor.white
         self.emailLabel.font = UIFont.systemFont(ofSize: 11)
         self.emailLabel.backgroundColor = UIColor.clear
-        
         headerView.addSubview(emailLabel)
         
-        let defaultProfile = UIImage(named: "account.jpg")
-        self.profileImage.image = defaultProfile
         self.profileImage.frame = CGRect(x: 10, y: 10, width: 50, height: 50)
-        
-        // 둥글게
         self.profileImage.layer.cornerRadius = (self.profileImage.frame.width / 2)
         self.profileImage.layer.borderWidth = 0
         self.profileImage.layer.masksToBounds = true
         view.addSubview(self.profileImage)
     }
     
-    // MARK: Data source
+    override func viewWillAppear(_ animated: Bool) {
+        self.nameLabel.text = self.uinfo.name ?? "Guest"
+        self.emailLabel.text = self.uinfo.account ?? ""
+        self.profileImage.image = self.uinfo.profile
+    }
+    
+    // MARK: - Data source
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.titles.count
     }
@@ -72,7 +71,7 @@ class SideBarVC: UITableViewController {
         return cell
     }
     
-    // MARK: Delegate
+    // MARK: - Delegate
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if indexPath.row == 0 {
             let uv = self.storyboard?.instantiateViewController(withIdentifier: "MemoForm")
